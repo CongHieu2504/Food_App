d4e38f33b4f2eb14798e216afbf910f1e5f8ea5b
Minh Hai <hainguyenminh.20039@gmail.com>
2024-09-13 11:49:42 +0700

    add file

--- FOOD_APP/App.js
+++ FOOD_APP/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello ahahhah</Text>
+      <Text>xin chaoào</Text>
       <StatusBar style="auto" />
     </View>
   );
